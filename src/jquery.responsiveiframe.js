ca63e385dad02ad69e9d5e8c31aa56b6852447ef
Eric Amodio <eamodio@gmail.com>
2013-02-23 16:04:51 -0500

    Attepted fix for IE7 (or browsers that don't support postMessage

--- src/jquery.responsiveiframe.js
+++ src/jquery.responsiveiframe.js
@@ -115,7 +115,7 @@ if (typeof jQuery !== 'undefined') {
     if(top.postMessage){
       top.postMessage( h , '*');
     } else {
-      window.location.hash = 'h'+h;
+      top.location.hash = 'h'+h;
     }
   };
 
